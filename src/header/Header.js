209a4e18996d1d3df04977dd0570331dd9789a48
Philip Stehlik <p@pstehlik.com>
2017-11-03 16:42:26 -0700

    look nicer, deployable to github pages

--- src/header/Header.js
+++ src/header/Header.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import { Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink, Collapse } from 'reactstrap';
 
+import './Header.css'
+
 class Header extends Component {
     render() {
         return (
             <Navbar color="faded" light expand="md">
-                <NavbarBrand href="/">Truffle Contract Interact</NavbarBrand>
+                <NavbarBrand href="/"><div class="spinner"> <div class="spinnerText">Truffle Contract Interact</div></div></NavbarBrand>
                 <NavbarToggler />
                 <Collapse isOpen={true} navbar>
                     <Nav className="ml-auto" navbar>
